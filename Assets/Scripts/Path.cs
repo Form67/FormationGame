fa67200ed2da1c2bf31e88151151c98b9dcc9b37
Jared Okun <okunj20@gmail.com>
2018-10-17 17:20:24 -0400

    imported code from prev hw

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+
+	public Vector3 origin;
+	public float amplitude;
+	public float frequency;
+	public float endX;
+	LineRenderer lineR;
+	public int lineSegments;
+
+	public List<Vector3> linePoints;
+	// Use this for initialization
+	void Start () {
+		lineR = GetComponent<LineRenderer> ();
+		lineR.positionCount = lineSegments + 1;
+		linePoints = new List<Vector3> ();
+		for (int i = 0; i <= lineSegments; ++i) {
+
+			Vector3 linePoint = getPoint (i / (float)lineSegments);
+			linePoints.Add (linePoint);
+			lineR.SetPosition (i, linePoint);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lineR = GetComponent<LineRenderer> ();
+		lineR.positionCount = lineSegments + 1;
+		linePoints = new List<Vector3> ();
+		for (int i = 0; i <= lineSegments; ++i) {
+
+			Vector3 linePoint = getPoint (i / (float)lineSegments);
+			linePoints.Add (linePoint);
+			lineR.SetPosition (i, linePoint);
+		}
+	}
+
+	public void Reset() {
+
+		origin = Vector3.zero;
+	}
+
+	public Vector3 getPoint(float t){
+		float y = amplitude * Mathf.Cos (frequency * t) + origin.y;
+		float x = (endX - origin.x) * t + origin.x;
+		return new Vector3 (x, y, 0);
+	}
+
+	/*public Vector3 getBezierPoint(float t) {
+		return Vector3.Lerp (Vector3.Lerp (Vector3.Lerp (bezierPoints [0], bezierPoints [1], t), Vector3.Lerp (bezierPoints [1], bezierPoints [2], t), t), 
+			Vector3.Lerp (Vector3.Lerp (bezierPoints [1], bezierPoints [2], t), Vector3.Lerp (bezierPoints [2], bezierPoints [3], t), t), t);
+	}*/
+}
---
+++ Assets/Scripts/SpawnFlock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFlock : MonoBehaviour {
+	public int flockSize;
+	public GameObject flockPrefab;
+	public float initialSeperation;
+	public Vector3 origin;
+
+
+	public List<GameObject> flockList;
+
+	// Use this for initialization
+	void Awake () {
+		flockList = new List<GameObject> ();
+		for(int i = 0; i < flockSize; i++){
+			GameObject flockInstance = Instantiate (flockPrefab);
+			Vector3 position = origin;
+			position.x += i * initialSeperation;
+			flockInstance.transform.position = position;
+			flockList.Add (flockInstance);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
