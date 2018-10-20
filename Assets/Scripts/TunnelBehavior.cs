73d98666abbc6a14ba0bd18f057eacfd4497afb3
Sam Gould <drmcman67@yahoo.com>
2018-10-20 15:59:29 -0400

    tunnel behavior begun

---
+++ Assets/Scripts/TunnelBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelBehavior : MonoBehaviour {
+    public GameObject[] boids;
+    public bool waitingForTurn;
+	// Use this for initialization
+	void Start () {
+        boids = GameObject.FindGameObjectsWithTag("boid");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void tunnel1() {
+
+    }
+}
