7dc1c9ffc5b692693efe0f1a5e38c868afc8cbfd
Kirsten Pilla <pillakirsten@gmail.com>
2018-10-20 12:28:05 -0400

    created movement base class
    working on moving code to scalable manager next

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    [Header("Basic Movement")]
+    public float maxAcceleration;
+    public float maxVelocity;
+
+    [Header("Dynamic Arrive")]
+    public float slowRadius;
+    public float timeToTarget;
+
+    protected Vector2 DynamicSeek(Vector3 position, Vector3 target)
+    {
+        Vector2 linearAcc = target - position;
+        return maxAcceleration * linearAcc;
+    }
+
+    protected Vector2 DynamicArrive(Vector3 position, Vector3 target, Vector2 currentVelocity)
+    {
+        float targetSpeed = maxVelocity * (Vector3.Distance(position, target) / slowRadius);
+        Vector2 directionVector = (target - position).normalized;
+        Vector2 targetVelocity = directionVector * targetSpeed;
+        Vector2 acceleration = (targetVelocity - currentVelocity) / timeToTarget;
+        return maxAcceleration * acceleration;
+    }
+}
