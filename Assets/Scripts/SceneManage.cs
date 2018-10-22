3cf8277c141a06488563cdf89ba2faa3a7bcbd86
Sam Gould <drmcman67@yahoo.com>
2018-10-22 19:00:35 -0400

    UI and emergent complete

---
+++ Assets/Scripts/SceneManage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Scalable() {
+        SceneManager.LoadScene("ScalableFormation");
+    }
+    public void Emergent() {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Level2() {
+        SceneManager.LoadScene("Level2");
+    }
+
+}
